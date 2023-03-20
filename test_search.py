e9d87de978c31cf75e031e733ef638259196a257
Kellerberg <r.carter.hpl@gmail.com>
2023-03-21 02:50:59 +0300

    Entering random search request added

--- test_search.py
+++ test_search.py
@@ -1,6 +1,16 @@
 import pytest
+import random
 from selene.support.shared import browser
 from selene import be, have
+
+random.seed()
+
+random_requst = ""
+def enter_random_request():
+    global random_requst
+    for x in range(10):
+        random_requst = random_requst + random.choice(list('12345678909qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLKZXZCVBNM'))
+    return random_requst
 
 @pytest.fixture
 def set_browser_configuration():
@@ -9,7 +19,12 @@ def set_browser_configuration():
     browser.driver.maximize_window()
     browser.config.hold_browser_open = True
 
-
+'''
 def test_search_positive(set_browser_configuration):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     assert browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+'''
+def test_search_negative(set_browser_configuration):
+    browser.element('[name="q"]').should(be.blank).type(enter_random_request()).press_enter()
+    assert browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
